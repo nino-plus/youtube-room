c73645c1312b90be52575fd652c8b1e542452c12
kokiinoue45 <koki_inoue45@icloud.com>
2020-09-21 19:39:57 +0900

    マージコミット

--- src/app/chats.service.ts
+++ src/app/chats.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { ActivatedRoute } from '@angular/router';
+import { firestore } from 'firebase';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Message } from './interfaces/message';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatsService {
 
-  constructor() { }
+  constructor(
+    private db: AngularFirestore,
+  ) { }
+
+  sendMessage(cannelId: string, uid: string, comments: string) {
+    const id = this.db.createId;
+    return this.db.doc(`rooms/${cannelId}/messages/${id}`).set({
+      uid,
+      comments,
+      createdAt: firestore.Timestamp.now()
+    });
+  }
+
+  getLatestMessages(cannelId: string): Observable<Message> {
+    return this.db
+    .collection<Message>(`rooms/${cannelId}/messages/`, ref => ref.orderBy('createdAt', 'desc').limit(1))
+    .valueChanges().pipe(
+      map(docs => docs && docs[0])
+    );
+  }
+
+  getChatsColumnMessages(cannelId: string, uid: string, comments: string) {
+    return this.db.collection(`rooms/${cannelId}/messages/`).add({
+      uid,
+      comments
+    });
+  }
 }
--- src/app/chats/chats.component.ts
+++ src/app/chats/chats.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { ChatsService } from '../chats.service';
+import { AuthService } from '../services/auth.service';
 import { RoomService } from '../services/room.service';
 
 @Component({
@@ -8,11 +12,38 @@ import { RoomService } from '../services/room.service';
 })
 export class ChatsComponent implements OnInit {
 
+  messages = {};
+  form = this.fb.group({
+    comments: ['', Validators.required]
+  });
+
+  private uid = this.authService.uid;
+  private channelId = this.route.snapshot.paramMap.get('id');
+
   constructor(
     public roomService: RoomService,
-  ) { }
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private chatService: ChatsService,
+    private route: ActivatedRoute,
+  ) {
+    // this.chatService.getLatestMessages(this.channelId).subscribe(messages => {
+    //   if (!this.messages[messages.uid]) {
+    //     this.messages[messages.comments] = [];
+    //   }
 
-  ngOnInit(): void {
+    //   this.messages[messages.uid].unshift(messages.comments);
+    // });
   }
 
+  ngOnInit(): void {
+
+  }
+  sendMessage() {
+    this.chatService.sendMessage(
+      'UCUPq5dKFGnOziaqYI-ejYcg',
+      this.uid,
+      this.form.value.comments
+    );
+  }
 }
