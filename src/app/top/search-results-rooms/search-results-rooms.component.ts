46d99d38ca881fbc967a309b1510e1c24fa1bef4
kokiinoue45 <koki_inoue45@icloud.com>
2020-09-22 21:01:36 +0900

    修正

--- src/app/top/search-results-rooms/search-results-rooms.component.ts
+++ src/app/top/search-results-rooms/search-results-rooms.component.ts
@@ -1,9 +1,8 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
 import { RoomService } from 'src/app/services/room.service';
-import { SearchRoomService } from 'src/app/services/search-room.service';
 
 @Component({
   selector: 'app-search-results-rooms',
@@ -12,13 +11,12 @@ import { SearchRoomService } from 'src/app/services/search-room.service';
 })
 export class SearchResultsRoomsComponent implements OnInit, OnDestroy {
   @Input() resultRoom: string;
+
   public searchText: string;
   private subscriptions: Subscription = new Subscription();
   private uid = this.authService.uid;
 
   constructor(
-    private route: ActivatedRoute,
-    private searchRoomService: SearchRoomService,
     private roomService: RoomService,
     private authService: AuthService,
     private router: Router,
@@ -40,7 +38,6 @@ export class SearchResultsRoomsComponent implements OnInit, OnDestroy {
     this.roomService.createRoom(channelId, title);
     this.roomService.addRoomMembers(channelId, this.uid).then(() => {
       this.router.navigateByUrl(`/room/${channelId}`);
-      this.roomService.getChannelVideos(channelId);
     });
   }
 }
