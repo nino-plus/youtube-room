ab1dfb1006ad121914a0770b1c0a09604c0a7b9f
Yusuke Gunji <inochideutae@gmail.com>
2020-09-22 14:19:45 +0900

    feat(create): createコンポーネント追加

---
+++ src/app/top/create/create.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { UserData } from 'src/app/interfaces/user';
+import { AuthService } from 'src/app/services/auth.service';
+import { UserService } from 'src/app/services/user.service';
+
+@Component({
+  selector: 'app-create',
+  templateUrl: './create.component.html',
+  styleUrls: ['./create.component.scss'],
+})
+export class CreateComponent implements OnInit {
+  avatarIds = [...Array(10)].map((_, i) => i + 1);
+  config: SwiperConfigInterface = {
+    loop: true,
+    navigation: true,
+    pagination: {
+      el: '.pager',
+      clickable: true,
+    },
+    centeredSlides: true,
+    slidesPerView: 3,
+  };
+  selectedId = 1;
+
+  user$: Observable<UserData> = this.authService.user$;
+  uid: string;
+
+  form = this.fb.group({
+    name: ['', [Validators.required, Validators.maxLength(40)]],
+  });
+
+  get nameControl() {
+    return this.form.get('name') as FormControl;
+  }
+
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    private userService: UserService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.user$
+      .pipe(
+        map((user) => {
+          return user?.uid;
+        })
+      )
+      .subscribe((uid) => {
+        this.uid = uid;
+      });
+  }
+
+  submit() {
+    const userName = this.form.value.name;
+    const avatarId = this.selectedId;
+    this.userService
+      .createUser(this.uid, userName, avatarId)
+      .then(() => this.router.navigateByUrl('/top'));
+    console.log(this.uid);
+    console.log(userName);
+    console.log(avatarId);
+  }
+}
--- src/app/top/top.module.ts
+++ src/app/top/top.module.ts
@@ -3,21 +3,29 @@ import { CommonModule } from '@angular/common';
 
 import { TopRoutingModule } from './top-routing.module';
 import { TopComponent } from './top/top.component';
-import { FormsModule } from '@angular/forms';
 import { SearchResultsRoomsComponent } from './search-results-rooms/search-results-rooms.component';
-import {MatButtonModule} from '@angular/material/button';
-import {MatDividerModule} from '@angular/material/divider';
-
-
+import { MatDividerModule } from '@angular/material/divider';
+import { SwiperModule } from 'ngx-swiper-wrapper';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { CreateComponent } from './create/create.component';
 
 @NgModule({
-  declarations: [TopComponent, SearchResultsRoomsComponent],
+  declarations: [TopComponent, SearchResultsRoomsComponent, CreateComponent],
   imports: [
     CommonModule,
     TopRoutingModule,
     FormsModule,
     MatButtonModule,
-    MatDividerModule
-  ]
+    MatDividerModule,
+    MatIconModule,
+    SwiperModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
 })
-export class TopModule { }
+export class TopModule {}
