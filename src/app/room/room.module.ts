e52629887372ab258440ac95fbbdba780e92efc7
peyo-code <mr233425@gmail.com>
2020-09-26 22:49:18 +0900

    feat(room): UI修正

--- src/app/room/room.module.ts
+++ src/app/room/room.module.ts
@@ -11,6 +11,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [RoomComponent],
@@ -26,6 +27,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     MatIconModule,
     MatButtonModule,
     MatProgressSpinnerModule,
+    MatToolbarModule
   ],
 })
 export class RoomModule { }
