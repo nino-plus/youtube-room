a0b9dcd7da824dd875549908045105cba8feb165
kokiinoue45 <koki_inoue45@icloud.com>
2020-09-26 18:41:24 +0900

    feat(loading): loadingUIの実装

--- src/app/interfaces/room.ts
+++ src/app/interfaces/room.ts
@@ -1,6 +1,9 @@
 export interface Room {
   id: string;
   title: string;
+  description: string;
+  thumbnailURL: string;
   allVideosCount: number;
   initialAction: boolean;
+  isCreating: boolean;
 }
