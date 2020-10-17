1c74182a4005f87c1b1ea1794ee8a370a0949bba
kokiinoue45 <koki_inoue45@icloud.com>
2020-10-18 00:38:58 +0900

    refactor(room-video): youtubeディスプレイコンポーネント作成

--- src/app/room/room-video/room-video.component.ts
+++ src/app/room/room-video/room-video.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { RoomService } from 'src/app/services/room.service';
 
 @Component({
   selector: 'app-room-video',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./room-video.component.scss']
 })
 export class RoomVideoComponent implements OnInit {
+  @Input() channelId: string;
 
-  constructor() { }
+  private player: YT.Player;
+  playerVars = {
+    controls: 0,
+  };
+
+  constructor(private roomService: RoomService) { }
 
   ngOnInit(): void {
+    this.roomService.getPlayVideo(this.channelId).pipe(
+      map(doc => doc && doc.videoId)
+    ).subscribe((videoId) => {
+      if (this.player) {
+        this.player.loadVideoById(videoId);
+      }
+    });
   }
 
+  savePlayer(player) {
+    this.player = player;
+    this.player.playVideo();
+  }
 }
